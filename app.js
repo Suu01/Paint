2b6c17a6169d8d0d3f462358b8fa1899f4e4b696
Suu01 <92988188+Suu01@users.noreply.github.com>
2024-01-10 00:42:17 +0900

    line

--- app.js
+++ app.js
@@ -1,32 +1,73 @@
+const colorOptions = Array.from(
+  document.getElementsByClassName("color-option")
+);
+const color = document.getElementById("color");
+const lineWidth = document.getElementById("line-width");
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d"); // brushes - 캔버스에 그림 그릴 때 사용
 
 canvas.width = 800;
 canvas.height = 800;
 
-ctx.lineWidth = 2;
+ctx.lineWidth = lineWidth.value;
+let isPainting = false;
 
-const colors = [
-  "#ff3838",
-  "#ffb8b8",
-  "#c56cf0",
-  "#ff9f1a",
-  "#fff200",
-  "#32ff7e",
-  "#7efff5",
-  "#18dcff",
-  "#7d5fff",
-];
-
-function onClick(event) {
-  // console.log(event);
+function onMove(event) {
+  if (isPainting) {
+    ctx.lineTo(event.offsetX, event.offsetY);
+    ctx.stroke();
+    return;
+  }
   ctx.beginPath();
-  ctx.moveTo(0, 0);
-  const color = colors[Math.floor(Math.random() * colors.length)];
-  ctx.strokeStyle = color;
-  ctx.lineTo(event.offsetX, event.offsetY);
-  ctx.stroke();
+  ctx.moveTo(event.offsetX, event.offsetY);
 }
 
-// canvas.addEventListener("click", onClick);
-canvas.addEventListener("mousemove", onClick);
+function startPainting(event) {
+  isPainting = true;
+}
+
+function cancelPainting(event) {
+  isPainting = false;
+  // ctx.beginPath();
+}
+
+function onLineWidthChange(event) {
+  // console.log(event.target.value);
+  ctx.lineWidth = event.target.value;
+}
+
+function onColorChange(event) {
+  // console.log(event.target.value);
+  ctx.strokeStyle = event.target.value;
+  ctx.fillStyle = event.target.value;
+}
+
+function onColorClick(event) {
+  // console.dir(event.target.dataset.color);
+  const colorValue = event.target.dataset.color;
+  ctx.strokeStyle = colorValue;
+  ctx.fillStyle = colorValue;
+  color.value = colorValue;
+}
+
+canvas.addEventListener("mousemove", onMove);
+canvas.addEventListener("mousedown", startPainting);
+canvas.addEventListener("mouseup", cancelPainting);
+canvas.addEventListener("mouseleave", cancelPainting);
+
+// document.addEventListener("mouseup", onMouseUp);
+
+lineWidth.addEventListener("change", onLineWidthChange);
+color.addEventListener("change", onColorChange);
+
+colorOptions.forEach((color) => color.addEventListener("click", onColorClick));
+
+// function onColorChange(event) {
+//   ctx.strokeStyle = ctx.fillStyle = event.target.value;
+// }
+
+// function onColorClick(event) {
+//   const colorValue = event.target.dataset.color;
+//   ctx.strokeStyle = ctx.fillStyle = event.target.dataset.color;
+//   color.value = colorValue;
+// }
